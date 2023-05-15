bdc1ee89c691babf3af9cc02880b6584c2efbbb2
JungYeon <72572045+Jungyeon-Choi-hubbb@users.noreply.github.com>
2023-05-15 22:21:32 +0900

    D1_2050

--- src/D1/D1_2050.java
+++ src/D1/D1_2050.java
@@ -6,7 +6,14 @@ public class D1_2050 {
     public static void main(String args[]) throws Exception {
         Scanner sc = new Scanner(System.in);
 
-        int T = sc.nextInt();
-
+        String T = sc.next();
+        if (T.length() > 0 && T.length() < 200) {
+        for (int j = 0; j < T.length(); j++) {
+            System.out.print(Integer.parseInt(String.valueOf(T.charAt(j) - 64)) + " ");
+            }
+        }
+        else {
+            System.out.println("에러");
+        }
     }
 }
