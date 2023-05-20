f2fc1795df975bb85cdcd9fc67f84e704d86c989
JungYeon <72572045+Jungyeon-Choi-hubbb@users.noreply.github.com>
2023-05-21 07:30:55 +0900

    D2 문제풀이

---
+++ src/D2/D2_1954.java
@@ -0,0 +1,25 @@
+package D2;
+
+import java.util.Scanner;
+
+public class D2_1954 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        for(int test_case=0; test_case<T; test_case++){
+            int N = sc.nextInt();
+
+            int[][] arr = new int[N][N];
+
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++) {
+                }
+            }
+
+        }
+
+
+    }
+}
